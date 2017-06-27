fa716a826e17e714b64fa560cea46b0c8de2cbd1
Beto <albertosveloso@gmail.com>
2017-06-26 23:56:19 -0300

    Tela sobre concluida faltando imagens

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -2,6 +2,8 @@ const { ipcRenderer } = require('electron');
 
 let linkSobre = document.querySelector('#link-sobre');
 
+console.log(linkSobre);
+
 linkSobre.addEventListener("click", function(){
     ipcRenderer.send('abrir-janela-sobre');
 });
