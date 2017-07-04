e55feb3831deb4e8ddee814f5dc946640c77905e
Beto-L <albertosveloso@gmail.com>
2017-07-04 15:13:24 -0300

    Botão play e stop, funcao array reverse

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -7,3 +7,11 @@ console.log(linkSobre);
 linkSobre.addEventListener("click", function(){
     ipcRenderer.send('abrir-janela-sobre');
 });
+
+let botaoPlay = document.querySelector('.botao-play');
+
+let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+botaoPlay.addEventListener("click", function(){
+    imgs = imgs.reverse(); //invertendo array
+    botaoPlay.src = imgs[0];
+});
