e79b1afa8ea8c8a0a5fb0ddfaec3bec9d18b4ee0
Beto <albertosveloso@gmail.com>
2017-07-15 11:07:25 -0300

    Parando o timer

--- app/js/timer.js
+++ app/js/timer.js
@@ -1,6 +1,6 @@
 const moment = require('moment'); //biblioteca especialista em trabalhar com data, horas etc...
-
 let segundos;
+let timer;
 
 //exportar objeto, criação de módulo node.js
 module.exports = {
@@ -8,12 +8,17 @@ module.exports = {
       let tempo = moment.duration(el.textContent);
       segundos = tempo.asSeconds();
 
+      clearInterval(timer); //evitando problemas de ficar acelerando o timer, limpa o timer anterior
+
      //para cada segundo passar colocar um setinterval para aumentar os segundos
      //necessário utilizar arrow function senão não vamos conseguir utilizar a função segundosParaTempo()
-     setInterval(()=>{
+     timer = setInterval(()=>{
        segundos++;
        el.textContent = this.segundosParaTempo(segundos);
      }, 1000);
+  },
+  parar(){
+    clearInterval(timer);
   },
   segundosParaTempo(segundos){
       //00:00:00
