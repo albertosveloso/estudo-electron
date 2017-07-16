b806cbe40bd62d8f6950715c03dfb40b3a537fd0
Beto <albertosveloso@gmail.com>
2017-07-16 17:42:07 -0300

    Campo para adicionar novo curso

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -6,6 +6,8 @@ let linkSobre = document.querySelector('#link-sobre');
 let botaoPlay = document.querySelector('.botao-play');
 let tempo = document.querySelector('.tempo');
 let curso = document.querySelector('.curso');
+let botaoAdicionar = document.querySelector('.botao-adicionar');
+let campoAdicionar = document.querySelector('.campo-adicionar');
 
 window.onload = () => {
 
@@ -33,4 +35,12 @@ botaoPlay.addEventListener("click", function(){
 
     imgs = imgs.reverse(); //invertendo array
     botaoPlay.src = imgs[0];
+});
+
+botaoAdicionar.addEventListener('click', function(){
+  let novoCurso = campoAdicionar.value;
+  curso.textContent = novoCurso;
+  tempo.textContent = '00:00:00';
+
+  campoAdicionar.value = '';
 });
